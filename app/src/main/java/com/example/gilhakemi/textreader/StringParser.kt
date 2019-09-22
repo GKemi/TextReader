ad528dde5530bbad190ce34ed81df6d91b443e3c
Gil Hakemi <gilhackemi@gmail.com>
2019-09-22 11:51:58 +0100

    remove punctuation from words

--- app/src/main/java/com/example/gilhakemi/textreader/StringParser.kt
+++ app/src/main/java/com/example/gilhakemi/textreader/StringParser.kt
@@ -3,10 +3,17 @@ package com.example.gilhakemi.textreader
 /**
  * Created by work on 22/09/2019.
  */
+import java.util.regex.Pattern
+
 class StringParser {
 
     fun parse(phrase: String): List<String> {
-        val words = phrase.split(" ")
+        val pattern = Pattern.compile("\\w+")
+        val matcher = pattern.matcher(phrase)
+        val words = mutableListOf<String>()
+        while (matcher.find()) {
+            words.add(matcher.group())
+        }
         return words
     }
 
