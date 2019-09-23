c5d01686210b6e2113cce3793c78c1795bf26617
Gil Hakemi <gilhackemi@gmail.com>
2019-09-23 19:14:39 +0100

    ensure view receives a list of words with associated number of occurrences

--- app/src/androidTest/java/com/example/gilhakemi/textreader/WordRetrievalTests.kt
+++ app/src/androidTest/java/com/example/gilhakemi/textreader/WordRetrievalTests.kt
@@ -1,8 +1,14 @@
 package com.example.gilhakemi.textreader
 
+import android.app.Application
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import android.app.Instrumentation.newApplication
+import android.content.Context
+import android.support.test.runner.AndroidJUnitRunner
+
+
 
 /**
  * Created by work on 22/09/2019.
@@ -22,4 +28,23 @@ class WordRetrievalTests {
         assertNotNull(view.words)
     }
 
+    @Test
+    fun viewRetrievesAListOfWords_withAssociatedOccurrences() {
+        //given there is a view
+        view = MockMainView()
+
+        //when the view requests for a text file
+        view.getWords()
+
+        //then a list of words should be returned with occurrences
+        assertEquals("the", view.words!![0].word)
+        assertEquals(3366, view.words!![0].occurrences)
+        assertEquals("railway", view.words!![1].word)
+        assertEquals(54, view.words!![1].occurrences)
+        assertEquals("children", view.words!![2].word)
+        assertEquals(157, view.words!![2].occurrences)
+    }
+
+
 }
+
